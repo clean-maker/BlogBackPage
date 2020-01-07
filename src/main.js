08aa52b501b13e6f79e848fc7090bd410be79f0e
cleanmaker <1399392831@qq.com>
2020-01-07 15:52:30 +0800

    第三次提交：改掉main.js警告问题  2020/01/07

--- src/main.js
+++ src/main.js
@@ -30,8 +30,9 @@ import * as filters from './filters' // global filters
  */
 import { mockXHR } from '../mock'
 // if (process.env.NODE_ENV === 'production') {
-mockXHR()
 // }
+mockXHR()
+
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
